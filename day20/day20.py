6dfc1e41a33232d3910f269762a406d0334b1153
mnopqr1 <78932387+mnopqr1@users.noreply.github.com>
2023-12-20 17:21:10 +0100

    day 20 done

--- day20/day20.py
+++ day20/day20.py
@@ -1,14 +1,7 @@
-
-
 from dataclasses import dataclass
+from functools import reduce
 from typing import Dict, List
 
-
-filename = "test.txt"
-with open(filename) as f:
-    ls = [l.rstrip() for l in f.readlines()]
-
-# False = low, True = high
 
 @dataclass
 class Module:
@@ -21,7 +14,7 @@ class Module:
 
     def handle(self, p):
         assert self.name == p.tow
-        print(f"{self.name} handles {p}")
+        # print(f"{self.name} handles {p}")
         newps = []
         tosend = False
         if self.ty == "b":
@@ -40,11 +33,10 @@ class Module:
             tosend = True
 
         if tosend:
-            print(f"Preparing to send to {self.ds}")
             for d in self.ds:
                 newp = Pulse(sendval, self.name, d)
                 newps.append(newp)
-                print(f"{self.name} sends {newp}")
+                if D: print(f"{self.name} sends {WORD[newp.value]} to {newp.tow}")
 
         return newps
 
@@ -54,9 +46,15 @@ class Pulse:
     value: bool
     cur: str
     tow: str
+    n_low = 0
+    n_high = 0
     n_pulses = 0
 
     def __init__(self, value=False, cur="button", tow="roadcaster"):
+        if value == True:
+            Pulse.n_high += 1
+        else:
+            Pulse.n_low += 1
         Pulse.n_pulses += 1
         self.n = Pulse.n_pulses
         self.value = value
@@ -67,8 +65,13 @@ class Pulse:
         return f"({self.n=}, {self.value=}, {self.cur=}, {self.tow=})"
 
 
-answer = 0
 module = dict()
+
+filename = "input.txt"
+with open(filename) as f:
+    ls = [l.rstrip() for l in f.readlines()]
+
+WORD = {False: "low", True: "high"}
 
 for l in ls:
     iden, dest = l.split(" -> ")
@@ -89,20 +92,33 @@ for n in module.keys():
         module[d].ins.append(n)
         module[d].mem[n] = False
 
-# for n in module.keys():
-#     print(module[n])
+done = False
+n_presses = 0
 
-p = Pulse()
-stack = [p]
+D = False
+D2 = False
 
-while len(stack) > 0:
-    p = stack.pop()
-    print(f"Current pulse: {p}")
-    handler = module[p.tow]
-    print(f"Current handler: {handler}")
-    newps = handler.handle(p)
-    stack += newps
-    print(stack)
-    input()
+ddins = module["dd"].ins
+firsttime = {d: None for d in ddins}
+while not done:
+    if D: print(f"------\npress button {n_presses+1}\n-----")
+    n_presses += 1
+    p = Pulse()
+    stack = [p]
 
-print(answer)
+    while len(stack) > 0:
+        p = stack.pop(0)
+        handler = module[p.tow]
+        newps = handler.handle(p)
+        for q in newps:
+            # if q.tow == "rx" and q.value == False:
+            #     done = True
+            # if D2:
+            if q.tow == "dd" and q.value == True and firsttime[q.cur] is None:
+                # print(f"{q.cur} sends high to dd at button press {n_presses}")
+                firsttime[q.cur] = n_presses
+        stack += newps
+    done = all(firsttime[d] is not None for d in firsttime.keys())
+
+# print(Pulse.n_low * Pulse.n_high)
+print(reduce(lambda x,y: x*y,firsttime.values()))
